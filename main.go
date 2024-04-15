0ad669c78ca1f35c75261c5ba3e7065f17985ed3
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-04-15 16:20:08 +0200

    refactor: add renderTemplate

--- main.go
+++ main.go
@@ -8,18 +8,35 @@ import (
 
 const port = ":8000"
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/home.page.html"))
-	tmpl.Execute(w, nil)
+type Film struct {
+	Title string
+	Director string
 }
 
-// func About(w http.ResponseWriter, r *http.Request) {
-	
-// }
+func renderTemplate(file string) *template.Template {
+	templ := template.Must(template.ParseFiles("templates/" + file))
+	return templ
+}
+
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	films := map[string][]Film{
+		"films": {
+			{Title: "The Shawshank Redemption", Director: "Frank Darabont"},
+			{Title: "The Godfather", Director: "Francis Ford Coppola"},
+			{Title: "The Dark Knight", Director: "Christopher Nolan"},
+		},
+	}
+	renderTemplate("home.page.html").Execute(w, films)
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplate("about.page.html").Execute(w, nil)
+}
 
 func main() {
 	http.HandleFunc("/", Home)
-	// http.HandleFunc("/about", About)
+	http.HandleFunc("/about", About)
 	fmt.Println("Server running on port", port)
 	http.ListenAndServe(port, nil)
 }

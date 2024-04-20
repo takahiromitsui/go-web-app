61807f538cce1419ed9a13a2c432116ab78614e2
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-04-20 09:35:15 +0200

    feat: create repo

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -3,19 +3,35 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/takahiromitsui/go-web-app/pkg/config"
 	"github.com/takahiromitsui/go-web-app/pkg/render"
 )
 
+// Repo the repository used by the handlers
+var Repo *Repository
 
-type Film struct {
-	Title string
-	Director string
+// Repository is the repository type
+type Repository struct{
+	App *config.AppConfig
 }
 
-func Home(w http.ResponseWriter, r *http.Request) {
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+func (m *Repository)Home(w http.ResponseWriter, r *http.Request) {
+	
 	render.RenderTemplate(w, "home.page.tmpl")
 }
 
-func About(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "about.page.tmpl")
 }

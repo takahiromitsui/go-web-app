153d1e1c001d7c63fc3a1214f799e1a07d3f07c9
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-04-20 09:14:56 +0200

    feat: create Config struct

---
+++ pkg/config/config.go
@@ -0,0 +1,13 @@
+package config
+
+import (
+	"html/template"
+	"log"
+)
+
+// AppConfig holds the application configuration
+type AppConfig struct {
+	UseCache      bool
+	TemplatesCache map[string]*template.Template
+	InfoLog       *log.Logger
+}

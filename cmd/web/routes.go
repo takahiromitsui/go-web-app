45777411e800e046652c7fa7d36146cebb5c4238
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-04-23 14:48:55 +0200

    feat: add Recoverer

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,12 +4,14 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/takahiromitsui/go-web-app/pkg/config"
 	"github.com/takahiromitsui/go-web-app/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
+	mux.Use(middleware.Recoverer)
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 

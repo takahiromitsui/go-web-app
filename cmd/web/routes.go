43971a5cced6e8689b896b871190b2f3bc57d2cd
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-04-23 14:43:43 +0200

    feat: add routers

---
+++ cmd/web/routes.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/takahiromitsui/go-web-app/pkg/config"
+	"github.com/takahiromitsui/go-web-app/pkg/handlers"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/takahiromitsui/go-web-app
 
 go 1.21.1
 
-require github.com/go-chi/chi/v5 v5.0.12 // indirect
+require github.com/go-chi/chi/v5 v5.0.12

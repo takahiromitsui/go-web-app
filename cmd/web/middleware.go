7de3899ea8330e85f8b70e07befaf55352580491
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-04-23 16:22:54 +0200

    feat: load and save session

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,20 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/justinas/nosurf"
 )
 
-
-func WriteToConsole(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Hit the page")
-		next.ServeHTTP(w, r)
-	})
-}
-
+// NoSurf adds CSRF protection to all POST requests
 func NoSurf(next http.Handler) http.Handler {
 	csrfHandler := nosurf.New(next)
 
@@ -26,4 +18,9 @@ func NoSurf(next http.Handler) http.Handler {
 	})
 
 	return csrfHandler
+}
+
+// SessionLoad loads and saves the session on every request
+func SessionLoad(next http.Handler) http.Handler {
+	return session.LoadAndSave(next)
 }

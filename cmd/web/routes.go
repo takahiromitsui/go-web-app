f8a9f98ba0432e43c0928e83ef41e54b9f2e9e57
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-04-24 15:30:13 +0200

    feat: add POST

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -19,6 +19,7 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/generals-quarters", handlers.Repo.Generals)
 	mux.Get("/majors-suite", handlers.Repo.Majors)
 	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Post("/search-availability", handlers.Repo.PostAvailability)
 	mux.Get("/contact", handlers.Repo.Contact)
 
 	fileServer := http.FileServer(http.Dir("./static/"))

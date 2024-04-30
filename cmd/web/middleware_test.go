af5c7350f6b087909bc8b18b36053cd03cea8aa7
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-04-30 15:21:28 +0200

    test: test session load

--- cmd/web/middleware_test.go
+++ cmd/web/middleware_test.go
@@ -16,3 +16,15 @@ func TestNoSurf(t *testing.T) {
 		t.Errorf("type is not http.Handler, but is %T", v) // test failed}
 	}
 }
+
+func TestSessionLoad(t *testing.T) {
+	var myH myHandler
+	h := SessionLoad(&myH)
+
+	switch v := h.(type) {
+	case http.Handler:
+		// do nothing; test passed
+	default:
+		t.Errorf("type is not http.Handler, but is %T", v) // test failed}
+	}
+}

3b5ed836e4572ed5585546ca032bd3cc455ea95f
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-04-30 10:44:42 +0200

    feat: store input

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -81,8 +81,14 @@ func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
 }
 
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	var emptyReservation models.Reservation
+	data := make(map[string]interface{})
+	data["reservation"] = emptyReservation
+
+
 	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
 		Form: forms.New(nil),
+		Data: data,
 	})
 }
 

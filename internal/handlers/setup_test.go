49bb0472ef5f856780a367d74c43b2a089c734b8
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-05-09 14:43:25 +0200

    chore: update tests

--- internal/handlers/setup_test.go
+++ internal/handlers/setup_test.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"path/filepath"
 	"time"
+	"os"
 
 	"github.com/alexedwards/scs/v2"
 	"github.com/go-chi/chi/v5"
@@ -28,6 +29,12 @@ func getRoutes() http.Handler {
 	gob.Register(models.Reservation{})
 	// change this to true when in production
 	app.InProduction = false
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
 
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour

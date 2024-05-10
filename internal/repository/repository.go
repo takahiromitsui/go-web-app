c1c4ccd9105927e01d9832b9ed8cd8621c8dd531
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-05-10 16:45:34 +0200

    feat: add InsertReservation

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,5 +1,8 @@
 package repository
 
+import "github.com/takahiromitsui/go-web-app/internal/models"
+
 type DatabaseRepo interface {
 	AllUsers() bool
+	InsertReservation(res models.Reservation) error
 }

a696be97d38e04f4901362ccbbbbab9d95356b2f
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-05-14 16:37:04 +0200

    feat: create SearchAvailabilityByDatesByRoomID

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -60,3 +60,29 @@ func (m *PostgresDBRepo) InsertRoomRestriction(res models.RoomRestriction) error
 	}
 	return nil
 }
+
+// SearchAvailabilityByDatesByRoomID returns true if availability exists for roomID, and false if no availability exists
+func (m *PostgresDBRepo) SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error) {
+	ctx,cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	query := `
+				select 
+					count(id)
+				from 
+					rooms+restrictions
+				where 
+					room_id = $1 
+					and	$2 < end_date and $3 > start_date;`
+	
+	var numRows int
+
+	row := m.DB.QueryRowContext(ctx, query, roomID, start, end)
+	err := row.Scan(&numRows)
+	if err != nil {
+		return false, err
+	}
+	if numRows == 0 {
+		return true, nil
+	}
+	return false, nil
+}
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -6,4 +6,5 @@ type DatabaseRepo interface {
 	AllUsers() bool
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(res models.RoomRestriction) error
+	SearchAvailabilityByDatesByRoomID(roomID int, startDate, endDate string) (bool, error)
 }

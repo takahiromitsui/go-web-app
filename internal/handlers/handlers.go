64c87e31a8988505dbf0a18f826779b2d403c3b6
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-05-15 13:57:35 +0200

    feat: create ChooseRoom handler

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -6,6 +6,8 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/go-chi/chi/v5"
+
 	"github.com/takahiromitsui/go-web-app/internal/config"
 	"github.com/takahiromitsui/go-web-app/internal/driver"
 	"github.com/takahiromitsui/go-web-app/internal/forms"
@@ -231,4 +233,20 @@ func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) 
 	render.Template(w, r, "reservation-summary.page.tmpl", &models.TemplateData{
 		Data: data,
 	})
+}
+
+func (m *Repository) ChooseRoom(w http.ResponseWriter, r *http.Request) {
+	roomID, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+	res, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
+	if !ok {
+		helpers.ServerError(w, err)
+		return
+	}
+	res.RoomID = roomID
+	m.App.Session.Put(r.Context(), "reservation", res)
+	http.Redirect(w, r, "/make-reservation", http.StatusSeeOther)
 }
